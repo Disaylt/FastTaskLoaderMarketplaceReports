558db2c0d627d77d3abe50a7da80466da54fc51d
disaylt <disofban@gmail.com>
2023-02-24 14:42:56 +0300

    feat: add api info model

--- FastTaskLoaderMarketplaceReports/Program.cs
+++ FastTaskLoaderMarketplaceReports/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FastTaskLoaderMarketplaceReports.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+string jsonWbContent = File.ReadAllText("wbSellers.json");
+List<ApiInfoModel>? wbSellers = JsonSerializer.Deserialize<List<ApiInfoModel>>(jsonWbContent);
