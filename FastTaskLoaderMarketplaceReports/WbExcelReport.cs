086cf861c59490c215c86fc2f29c241b9ba00bd8
disaylt <disofban@gmail.com>
2023-02-24 16:02:23 +0300

    feat: add excel

---
+++ FastTaskLoaderMarketplaceReports/WbExcelReport.cs
@@ -0,0 +1,31 @@
+﻿using ClosedXML.Excel;
+using FastTaskLoaderMarketplaceReports.Models.Wb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastTaskLoaderMarketplaceReports
+{
+    internal class WbExcelReport
+    {
+        private readonly List<WbStockModel> _wbStocks;
+
+        public WbExcelReport(List<WbStockModel> wbStocks)
+        {
+            _wbStocks = wbStocks;
+        }
+
+        public void CreateExcel(string fileName)
+        {
+            IXLWorkbook xLWorkbook = new XLWorkbook();
+            IXLWorksheet workSheet = xLWorkbook.Worksheet("Stats");
+            foreach(WbStockModel wbStock in _wbStocks)
+            {
+                
+            }
+
+        }
+    }
+}
